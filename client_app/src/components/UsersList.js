81db699ee122f5ac89b73694a8a13d31672372b7
Damian Enriquez <denriquez@tvapge.com>
2019-03-19 09:30:51 -0700

    small refactor don Userlist component

--- client_app/src/components/UsersList.js
+++ client_app/src/components/UsersList.js
@@ -12,40 +12,44 @@ const GET_USERS = gql`
   }
 `;
 
-const UsersList = () => (
-  <Query query={GET_USERS}>
-    {({ loading, error, data }) => {
-      if (loading) return 'Loading...';
-      if (error) return `Error! ${error.message}`;
+class UsersList extends React.Component {
+  render() {
+    return (
+      <Query query={GET_USERS}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
 
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-          {data.users.map(user => (
-            <tr key={user.id}>
-              <td>
-                {user.id}
-              </td>
-              <td>
-                {user.name}
-              </td>
-              <td>
-                {user.email}
-              </td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      );
-    }}
-  </Query>
-);
+          return (
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.users.map(user => (
+                  <tr key={user.id}>
+                    <td>
+                      {user.id}
+                    </td>
+                    <td>
+                      {user.name}
+                    </td>
+                    <td>
+                      {user.email}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          );
+        }}
+      </Query>
+    )
+  }
+}
 
 export default UsersList;
